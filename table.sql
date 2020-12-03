51353dbe210f7c84ad862cd0979e09ed7f0b3a84
ziyanda <ziyandandlamafa47@gmail.com>
2020-12-03 15:12:00 +0200

    file added

--- table.sql
+++ table.sql
@@ -1,7 +1,6 @@
 
 create table provinces(
     id serial not null primary key,
-    province_id int,
     contact_details text not null,
     branch text not null,
     email_address text not null,
@@ -14,7 +13,6 @@ create table provinces(
 
 create table client (
 	id serial not null primary key,
-    province_id int,
     contact_details text not null,
     branch text not null,
     email_address text not null,
@@ -22,10 +20,10 @@ create table client (
     business_tell text not null,
     cell text not null,
     status text not null,
-	foreign key (category_id) references categories(id)
+	foreign key (province_id) references provinces(id)
 );
 
-insert into provinces (province_id,contact_details,branch,email_address,postal_code,business_tell,cell,status)values 
+insert into provinces (contact_details,branch,email_address,postal_code,business_tell,cell,status)values 
 ('Yaasier Haroun','Wynberg','wynbergbranch@credit-rescue.co.za','64 Main Road, 2nd Floor, Old Absa Building','7800','217620039','N/A','Active');
 -- insert into provinces (province_id,contact_details,branch,email_address,postal_code,business_tell,cell,status)values ('ARTHUR RANDALL','Vredenburg','Unit 4, Medical Centre, Hill Street, 2nd Floor, Old Absa Building64 Main Road, 2nd Floor, Old Absa Building','leon.gm@credit-rescue.co.za','7380','223000186','0796064171/0731797919','Active');
 
