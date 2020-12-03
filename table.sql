0e6339dd9e980ef59d4a36cb85846117955cbd67
ziyanda <ziyandandlamafa47@gmail.com>
2020-12-03 15:20:31 +0200

    file added

--- table.sql
+++ table.sql
@@ -1,6 +1,7 @@
 
 create table provinces(
     id serial not null primary key,
+    province_id int,
     contact_details text not null,
     branch text not null,
     email_address text not null,
@@ -8,11 +9,12 @@ create table provinces(
     business_tell text not null,
     cell text not null,
     status text not null,
-	foreign key (contact_details) references contact_details(id)
+	foreign key (province_id) references provinces(id)
 );
 
 create table client (
 	id serial not null primary key,
+    province_id int,
     contact_details text not null,
     branch text not null,
     email_address text not null,
@@ -20,11 +22,11 @@ create table client (
     business_tell text not null,
     cell text not null,
     status text not null,
-	foreign key (contact_details) references contact_details(id)
+	foreign key (category_id) references categories(id)
 );
 
-insert into provinces (contact_details,branch,email_address,postal_code,business_tell,cell,status)values 
-('Yaasier Haroun','Wynberg','wynbergbranch@credit-rescue.co.za','64 Main Road, 2nd Floor, Old Absa Building','7800','217620039','N/A','Active');
+insert into provinces (province_id,contact_details,branch,email_address,postal_code,business_tell,cell,status)values 
+('1','Yaasier Haroun','Wynberg','wynbergbranch@credit-rescue.co.za','64 Main Road, 2nd Floor, Old Absa Building','7800','217620039','N/A','Active');
 -- insert into provinces (province_id,contact_details,branch,email_address,postal_code,business_tell,cell,status)values ('ARTHUR RANDALL','Vredenburg','Unit 4, Medical Centre, Hill Street, 2nd Floor, Old Absa Building64 Main Road, 2nd Floor, Old Absa Building','leon.gm@credit-rescue.co.za','7380','223000186','0796064171/0731797919','Active');
 
 -- alter table cateprovincgories add constraint uniq_desc_constraint unique(description);
